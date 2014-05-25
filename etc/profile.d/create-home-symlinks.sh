2ea558e513b7b0da4ee0a40660008d55d4f74c35
David Edwards <david@more.fool.me.uk>
2014-05-25 18:44:06 +0100

    Create create-home-symlinks.sh

---
+++ etc/profile.d/create-home-symlinks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Location of directories to symlink
+LOCATION="/mnt/bucket/$USER"
+
+declare -a DIRS=("Desktop" "Documents" "Downloads" "Music" "Pictures" "Public" "Templates" "Videos")
+
+NOW=$(date +"%Y%m%d%H%M%S")
+
+## Loop around the list of folders
+for DIR in "${DIRS[@]}"
+do
+   if ! [ -L "/home/$USER/$DIR" ]; then
+     if [ -d "/home/$USER/$DIR" ] || [ -f "/home/$USER/$DIR" ]; then
+         # If there is a directory of folder of the same name, move it out of the way
+         mv "/home/$USER/$DIR" "/home/$USER/$DIR.$NOW"
+     fi
+     # Create the symlink
+     ln -s "$LOCATION/$DIR" "/home/$USER/$DIR"
+   fi
+done
