bed660858fa204ade85e4c7929f60396593cb26d
David Edwards <david@more.fool.me.uk>
2014-06-05 18:48:12 +0100

    Fixed to work using /usr/sh

--- etc/profile.d/create-home-symlinks.sh
+++ etc/profile.d/create-home-symlinks.sh
@@ -1,21 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
 # Location of directories to symlink
-LOCATION="/mnt/bucket/$USER"
+LOCATION="/mnt/bucket/${USER}"
 
-declare -a DIRS=("Desktop" "Documents" "Downloads" "Music" "Pictures" "Public" "Templates" "Videos")
+DIRS="Desktop Documents Downloads Music Pictures Public Templates Videos"
 
 NOW=$(date +"%Y%m%d%H%M%S")
 
 ## Loop around the list of folders
-for DIR in "${DIRS[@]}"
+for DIR in $DIRS
 do
-   if ! [ -L "/home/$USER/$DIR" ]; then
-     if [ -d "/home/$USER/$DIR" ] || [ -f "/home/$USER/$DIR" ]; then
+   if ! [ -L "/home/${USER}/${DIR}" ]; then
+     if [ -d "/home/${USER}/${DIR}" ] || [ -f "/home/${USER}/${DIR}" ]; then
          # If there is a directory of folder of the same name, move it out of the way
-         mv "/home/$USER/$DIR" "/home/$USER/$DIR.$NOW"
+         mv "/home/${USER}/${DIR}" "/home/${USER}/${DIR}.${NOW}"
      fi
      # Create the symlink
-     ln -s "$LOCATION/$DIR" "/home/$USER/$DIR"
+     ln -s "${LOCATION}/${DIR}" "/home/${USER}/${DIR}"
    fi
 done
+
